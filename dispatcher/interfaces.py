1a2e77c3d72c559c5fdc7f0f1fd3b874dd1e938f
Restyled.io <commits@restyled.io>
2024-04-15 07:32:27 +0000

    Restyled by black

--- dispatcher/interfaces.py
+++ dispatcher/interfaces.py
@@ -5,7 +5,7 @@ import json
 from email.mime.text import MIMEText
 
 
-class mysql():
+class mysql:
 
     def __init__(self, torndb):
         # This is how we infer the connection
@@ -15,14 +15,12 @@ class mysql():
     # Main Handler will write a log of this
     def authorize_agent(self, auth):
         # or the query returnes more than 1 row
-        q = self.db.query("SELECT id " +
-                          "FROM agents " +
-                          "WHERE auth = %s", auth)
+        q = self.db.query("SELECT id " + "FROM agents " + "WHERE auth = %s", auth)
         # If we didn't get exactly 1 answer, there is a problem
         if len(q) == 0:
             return None
         elif len(q) == 1:
-            return q[0]['id']
+            return q[0]["id"]
         else:
             # TODO: LOG THIS problem. More than 2 results fetched
             return None
@@ -30,20 +28,22 @@ class mysql():
     # For all available jobs return their {id:x, 'fileset_scan'}
     # in a json list
     def fetch_available_jobs(self):
-        q = self.db.query("SELECT id, description " +
-                          "FROM jobs " +
-                          "WHERE `status` = 'new'")
+        q = self.db.query(
+            "SELECT id, description " + "FROM jobs " + "WHERE `status` = 'new'"
+        )
         our_answer = list()
         for entry in q:
             try:
-                entry_description = json.loads(entry['description'])
-                if 'fileset_scan' in entry_description:
+                entry_description = json.loads(entry["description"])
+                if "fileset_scan" in entry_description:
                     # If fileset scan is indeed in our description, then we add
                     # this entry in our answer
-                    our_answer.append({
-                        'id': entry['id'],
-                        'fileset_scan': entry_description['fileset_scan']
-                    })
+                    our_answer.append(
+                        {
+                            "id": entry["id"],
+                            "fileset_scan": entry_description["fileset_scan"],
+                        }
+                    )
             except Exception:
                 continue
         return json.dumps(our_answer)
@@ -57,28 +57,35 @@ class mysql():
         self.db.execute("BEGIN")
         # Let's see if the current job has been assigned meanwhile
         # We use SELECT FOR UPDATE to lock the entire row!
-        q = self.db.query("SELECT id, status, rules \
+        q = self.db.query(
+            "SELECT id, status, rules \
                             FROM jobs \
-                            WHERE `id` = %s FOR UPDATE", job_id)
+                            WHERE `id` = %s FOR UPDATE",
+            job_id,
+        )
         # There will be one answer or 0 for sure, due to index on id
         answer = dict()
         # Check the answer number
         if len(q) == 1:
             # Job still exists!
 
-            if q[0]['status'] != "new":
+            if q[0]["status"] != "new":
                 # Job has been already assigned!
-                answer['status'] = "rejected"
+                answer["status"] = "rejected"
             else:
                 # Job is available! Let's fetch it!
                 # Marking the job as assigned
-                self.db.execute("UPDATE jobs \
+                self.db.execute(
+                    "UPDATE jobs \
                                 SET `status` = 'assigned', `agent_id` = %s \
-                                WHERE `id` = %s", agent_id, job_id)
-                answer['status'] = "accepted"
-                answer['rules'] = q[0]['rules']
+                                WHERE `id` = %s",
+                    agent_id,
+                    job_id,
+                )
+                answer["status"] = "accepted"
+                answer["rules"] = q[0]["rules"]
         else:
-            answer['status'] = "rejected"
+            answer["status"] = "rejected"
 
         # Commit transaction!
         self.db.execute("COMMIT")
@@ -88,22 +95,25 @@ class mysql():
     # (notify_email, rules)
     # for a specific job
     def job_get_details(self, job_id):
-        assert (isinstance(job_id, int))
+        assert isinstance(job_id, int)
 
-        q = self.db.query("SELECT description, rules \
+        q = self.db.query(
+            "SELECT description, rules \
                             FROM jobs \
-                            WHERE `id` = %s LIMIT 1", job_id)
+                            WHERE `id` = %s LIMIT 1",
+            job_id,
+        )
         if len(q) == 1:
             notify_email = None
             # Job still exists!
-            job_description = json.loads(q[0]['description'])
-            if 'notify_email' in job_description:
-                notify_email = job_description['notify_email']
+            job_description = json.loads(q[0]["description"])
+            if "notify_email" in job_description:
+                notify_email = job_description["notify_email"]
 
             # If we want to return more data, we can do it here
 
             # Return the tuple
-            return (notify_email, q[0]['rules'])
+            return (notify_email, q[0]["rules"])
         return (None, None)
 
     # Function used to INSERT into db the list of results it receives
@@ -111,15 +121,18 @@ class mysql():
     def save_agent_results(self, agent_id, results, job_status):
         validators.validate_agent_results(results)
         agent_id = str(int(agent_id))
-        job_id = str(int(results['job_id']))
+        job_id = str(int(results["job_id"]))
 
         # Let's get the current job description
         self.db.execute("BEGIN")
         # Let's see if the current job has been assigned meanwhile
         # We use SELECT FOR UPDATE to lock the entire row!
-        q = self.db.query("SELECT * \
+        q = self.db.query(
+            "SELECT * \
                             FROM jobs \
-                            WHERE `id` = %s FOR UPDATE", job_id)
+                            WHERE `id` = %s FOR UPDATE",
+            job_id,
+        )
 
         # Now we append to description the execution time
         if len(q) == 1:
@@ -127,42 +140,49 @@ class mysql():
             job = q[0]
             # From description and update some values (if needed!)
             # Convert the job description into dict()
-            job_description = json.loads(job['description'])
+            job_description = json.loads(job["description"])
             # Add execution time + any errors / warnings reported by yara
-            job_description['execution_time'] = results['execution_time']
-            job_description['yara_errors'] = results['yara_errors']
-            job_description['yara_warnings'] = results['yara_warnings']
+            job_description["execution_time"] = results["execution_time"]
+            job_description["yara_errors"] = results["yara_errors"]
+            job_description["yara_warnings"] = results["yara_warnings"]
 
             # Insert the hashes into DB!
             # Convert from json to dict
             try:
-                md5_hashes = json.loads(results['md5_results'])
+                md5_hashes = json.loads(results["md5_results"])
                 # Now that we have a list of md5 hashes, add them to DB
                 for md5 in md5_hashes:
-                    self.db.execute("INSERT IGNORE into jobs_hashes (`job_id`, `hash_md5`) VALUES (%s, %s)",
-                                    job_id,
-                                    md5)
+                    self.db.execute(
+                        "INSERT IGNORE into jobs_hashes (`job_id`, `hash_md5`) VALUES (%s, %s)",
+                        job_id,
+                        md5,
+                    )
                 # Now let's insert the actual job!
-                self.db.execute("UPDATE jobs \
+                self.db.execute(
+                    "UPDATE jobs \
                                 SET `description` = %s,\
                                     `results`= %s, \
                                     `matched_files` = %s, \
                                     `status` = %s, \
                                     `finish_time` = %s \
                                 WHERE `id` = %s",
-                                json.dumps(job_description, ensure_ascii=True),
-                                results['yara_results'],
-                                results['matched_files'],
-                                job_status,
-                                results['finish_time'],
-                                job_id)
+                    json.dumps(job_description, ensure_ascii=True),
+                    results["yara_results"],
+                    results["matched_files"],
+                    job_status,
+                    results["finish_time"],
+                    job_id,
+                )
             except ValueError:
                 # We got an error while extracting the md5s
                 logging.error(
-                    'Could not parse the JSON with md5s received from worker: %s', results['md5_results'])
+                    "Could not parse the JSON with md5s received from worker: %s",
+                    results["md5_results"],
+                )
             except Exception as e:
                 logging.error(
-                    'General failure when trying to insert the job in db: %s', e)
+                    "General failure when trying to insert the job in db: %s", e
+                )
 
         else:
             # If the current job doesn't exist any more, we just pass,
@@ -173,22 +193,22 @@ class mysql():
         self.db.execute("COMMIT")
 
 
-class notification():
+class notification:
     # def __init__(self):
 
     def email(self, data):
-        assert (isinstance(data, dict))
+        assert isinstance(data, dict)
 
-        mail = MIMEText(data['body'])
-        mail['From'] = config.notification_email_from
-        mail['To'] = data['to']
-        mail['Subject'] = data['subject']
-        if len(data['to']) > 0 and config.notification_email_enabled:
+        mail = MIMEText(data["body"])
+        mail["From"] = config.notification_email_from
+        mail["To"] = data["to"]
+        mail["Subject"] = data["subject"]
+        if len(data["to"]) > 0 and config.notification_email_enabled:
             try:
                 smtp = smtplib.SMTP(config.notification_email_smtp_srv)
-                smtp.sendmail(config.notification_email_from,
-                              data['to'],
-                              mail.as_string())
+                smtp.sendmail(
+                    config.notification_email_from, data["to"], mail.as_string()
+                )
                 smtp.quit()
                 return True
             # We are catching all exceptions, since we are running in daemon
@@ -197,20 +217,20 @@ class notification():
                 return False
 
 
-class validators():
+class validators:
 
     @classmethod
     def validate_agent_results(cls, entry):
-        assert (isinstance(entry, dict))
-        assert ('job_id' in entry)
-        assert ('finish_time' in entry)
-        assert ('fileset_scan' in entry)
-        assert ('execution_time' in entry)
-        assert ('yara_errors' in entry)
-        assert ('yara_warnings' in entry)
-        assert ('matched_files' in entry)
-        assert ('md5_results' in entry)
+        assert isinstance(entry, dict)
+        assert "job_id" in entry
+        assert "finish_time" in entry
+        assert "fileset_scan" in entry
+        assert "execution_time" in entry
+        assert "yara_errors" in entry
+        assert "yara_warnings" in entry
+        assert "matched_files" in entry
+        assert "md5_results" in entry
 
         # And finally!
-        assert ('yara_results' in entry)
+        assert "yara_results" in entry
         return
--- worker/api_interface.py
+++ worker/api_interface.py
@@ -11,34 +11,36 @@ import json
 def fetch_available_jobs():
     # logging.info ("Called fetch_new_job")
     # Set up the request
-    payload = {'auth': config.api_key}
+    payload = {"auth": config.api_key}
     # Make the request, with the auth as POST DATA
     try:
-        r = requests.post(config.api_location +
-                          "/worker_fetch_available_jobs", data=payload)
+        r = requests.post(
+            config.api_location + "/worker_fetch_available_jobs", data=payload
+        )
         return r.json()
     # We want to catch any exception related to:
     # Not being able to connect (Requests exception) or json decodification
     except Exception as e:
-        logging.error('Exception: %s', e)
+        logging.error("Exception: %s", e)
         return json.loads('{"status": "error", "status_msg": "connection_error"}')
+
 
 # After fetching the available jobs, ask dispatcher to assign us one job
 
 
 def request_assign_job(job_id=-1):
     # Set up the request
-    payload = {'auth': config.api_key, 'job_id': job_id}
+    payload = {"auth": config.api_key, "job_id": job_id}
     # Make the request, with the auth as POST DATA
     try:
-        r = requests.post(config.api_location +
-                          "/worker_assign_job", data=payload)
+        r = requests.post(config.api_location + "/worker_assign_job", data=payload)
         return r.json()
     # We want to catch any exception related to:
     # Not being able to connect (Requests exception) or json decodification
     except Exception as e:
-        logging.error('Exception: %s', e)
+        logging.error("Exception: %s", e)
         return json.loads('{"status": "error", "status_msg": "connection_error"}')
+
 
 # Function used to push the results back
 
@@ -48,12 +50,11 @@ def push_results(results):
     # Set up the request
     # In order to set the 2 POST params, we use a dictionary,
     # And to send the "results" POST variable, we encode the contents into JSON
-    payload = {'auth': config.api_key, "results": json.dumps(results)}
+    payload = {"auth": config.api_key, "results": json.dumps(results)}
     # Make the request, with the auth as POST DATA
     try:
-        r = requests.post(config.api_location +
-                          "/worker_save_results", data=payload)
+        r = requests.post(config.api_location + "/worker_save_results", data=payload)
         return r.status_code
     except Exception as e:
-        logging.error('Exception: %s', e)
+        logging.error("Exception: %s", e)
         return 500
